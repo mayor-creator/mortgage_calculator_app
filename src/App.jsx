9fb2ad4bad14c730360f4e7fbcdc36772cffa6b6
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-11-14 14:44:56 -0500

    add style properties to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { FormContextProvider } from "./components/Context/FormContext.jsx";
 import { Form } from "./components/Form/Form.jsx";
+import styles from "./App.module.css";
 
 export default function App() {
   return (
     <>
       <FormContextProvider>
-        <main>
+        <main className={styles.main}>
           <Form />
         </main>
       </FormContextProvider>
